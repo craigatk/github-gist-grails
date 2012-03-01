55041bfbd74a103a1ea90b6825fb563619f308e9
Craig Atkinson <craig@practilogic.com>
2012-02-29 18:29:46 -0600

    Adding test app

---
+++ test/apps/gisttestapp201/grails-app/controllers/TestController.groovy
@@ -0,0 +1,7 @@
+class TestController {
+  // <gist>
+  def action1() {
+    println "In action 1"
+  }
+  // </gist>
+}
---
+++ test/apps/gisttestapp201/grails-app/services/TestService.groovy
@@ -0,0 +1,9 @@
+// <gist>
+class TestService {
+
+  def serviceMethod() {
+    println "In service method"
+  }
+
+}
+// </gist>

88d26aadfa0b18b832eafe670ec550d6ea52fe30
Craig Atkinson <craig@practilogic.com>
2012-03-09 20:25:46 -0600

    Adding SCM and issue tracking info

--- GithubGistGrailsPlugin.groovy
+++ GithubGistGrailsPlugin.groovy
@@ -51,10 +51,10 @@ Uploads code snippets from a Grails project to GitHub Gists so you can easily sh
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/craigatk/github-gist-grails/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "https://github.com/craigatk/github-gist-grails" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
